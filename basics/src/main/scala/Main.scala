afa915bebd17b2957d3387412b9b64b22a004e36
Martins Abele <martins.abelis@gmail.com>
2020-09-24 18:21:11 +0300

    added implementation of lcm and gcd

--- basics/src/main/scala/Main.scala
+++ basics/src/main/scala/Main.scala
@@ -1,3 +1,14 @@
+// Homework. Implement functions that calculate
+// https://en.wikipedia.org/wiki/Lowest_common_denominator and
+// https://en.wikipedia.org/wiki/Greatest_common_divisor for integers.
+
 object Main extends App {
-  println("Hello, World!")
+  def lcm(a: Int, b: Int): Int = {
+    (a*b).abs/gcd(a,b)
+  }
+
+  def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a.abs else gcd(b, a % b)
+  }
+
 }
